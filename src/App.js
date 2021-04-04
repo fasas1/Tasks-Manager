6a48808b76174ac39dea8e88b5a53e870eae101a
Fasasi Qasim <fasaqa1000@gmail.com>
2021-04-04 23:48:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState, useEffect } from 'react'
+import AddTask from './components/AddTask.js'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([] )
+
+
+  useEffect (() => {
+     const getTasks = async () => {
+        const tasksFromServer = await fetchTasks();
+         setTasks(tasksFromServer)
+     }
+    
+ getTasks()
+  }, [])
+
+
+  //fETCH TASKS
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+     return data
+ }
+
+
+//Add Task
+ const addTask = async (task)  => {
+   const res =  await fetch('http://localhost:5000/tasks/${id}', {
+    method:'POST',
+    headers:{
+      'Content-type':'application/json'
+    },
+     body: JSON.stringify(task)
+    
+ })
+
+    const data =  await res.json
+    setTasks([...tasks, data])
+ }
+
+
+//Delete Task
+ const deleteTask = async (id) => {
+    await fetch('http://localhost:5000/tasks/${id}', {
+       method:'DELETE'
+    })
+    setTasks(tasks.filter((task)  => task.id !== id ))
+ }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+       setTasks(tasks.map((task) => task.id === id ?
+        {...task, reminder: 
+        !task.reminder }:task ))
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+         <Header title = "Task Tracker"
+          onAdd={() => setShowAddTask(!showAddTask)} 
+          showAdd={showAddTask} 
+          />
+        {showAddTask && <AddTask  onAdd ={addTask} /> }
+         <Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder}/>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+
+
+const Button = ({ color, onClick, text}) => {
+
+    return (
+        <button onClick={onClick} style={{backgroundColor:color}} className= "btn">{text}</button>
+
+    ) 
+}
+
+export default Button
